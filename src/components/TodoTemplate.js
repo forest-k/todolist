3ff0001b36879bb74911b81974083f34f1044c0b
forest-k <bstnqls@naver.com>
2021-02-03 17:58:08 +0900

    프로젝트 업로드 테스트4

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Test = styled.div`
   width: 20px;
   height: 20px;
-  background: black;
+  background: yellow;
 `;
 
 const TodoTemplateWrap = styled.div`
